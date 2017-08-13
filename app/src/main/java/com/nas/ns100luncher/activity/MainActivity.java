590f2415247d9ba5e4a3cd5bf069cce46e259ab4
yijian2033 <787809032@qq.com>
2017-08-13 23:15:41 +0800

    增加跳转到原始的SETTING和文件管理

--- app/src/main/java/com/nas/ns100luncher/activity/MainActivity.java
+++ app/src/main/java/com/nas/ns100luncher/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.nas.ns100luncher.activity;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -72,16 +74,43 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_video:
-                VideoActivity.startAc(this);
+                Intent video = new Intent(Intent.ACTION_GET_CONTENT);
+//                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+//              intent.setType("image/*");
+//              intent.setType(“audio/*”); //选择音频
+                video.setType("video/*"); //选择视频 （mp4 3gp 是android支持的视频格式）
+//              intent.setType(“video/*;image/*”);//同时选择视频和图片
+                video.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(video, 1);
+//                VideoActivity.startAc(this);
                 break;
             case R.id.bt_picture:
-                PictureActivity.startAc(this);
+                Intent image = new Intent(Intent.ACTION_GET_CONTENT);
+//                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+              image.setType("image/*");
+//              intent.setType(“audio/*”); //选择音频
+//                video.setType("video/*"); //选择视频 （mp4 3gp 是android支持的视频格式）
+//              intent.setType(“video/*;image/*”);//同时选择视频和图片
+                image.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(image, 1);
+//                PictureActivity.startAc(this);
                 break;
             case R.id.bt_folder:
-                FolderActivity.startAc(this);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+//              intent.setType("image/*");
+//              intent.setType(“audio/*”); //选择音频
+//              intent.setType(“video/*”); //选择视频 （mp4 3gp 是android支持的视频格式）
+//              intent.setType(“video/*;image/*”);//同时选择视频和图片
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, 1);
+
+//                FolderActivity.startAc(this);
                 break;
             case R.id.bt_set:
-                SetActivity.startAc(this);
+                Intent setIntent = new Intent(Settings.ACTION_SETTINGS);
+                startActivity(setIntent);
+//                SetActivity.startAc(this);
                 break;
             case R.id.bt_more:
                 MoreActivity.startAc(this);
